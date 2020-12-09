f39e9dcf5d7068f1f223a563e5ac433a743fbb24
W1r3W0lf <apple.link.level@gmail.com>
2020-12-08 17:47:49 -0700

    upload code

---
+++ main.c
@@ -0,0 +1,80 @@
+#include "msp.h"
+//#include "time.h"
+#include "stdlib.h"
+
+
+enum mode {MASTER, SLAVE};
+
+#define board_mode MASTER;
+
+#define LED BIT0
+#define LED2RED BIT0
+#define LED2GREEN BIT1
+#define LED2BLUE BIT2
+
+
+// The states that the game could be in
+enum state {start_mode, game_mode, fail_mode};
+
+#define game_mode start;
+
+void restart();
+void game();
+
+// It's about a second per sec
+void sleep(unsigned int sec){
+    while(sec-- > 0)
+    {
+        int tick = 7;
+        while(tick-- > 0)
+        {
+            unsigned int delay = 65535;
+            while(delay-- > 0);
+        }
+    }
+}
+
+/**
+ * main.c
+ */
+void main(void)
+{
+	WDT_A->CTL = WDT_A_CTL_PW | WDT_A_CTL_HOLD;		// stop watchdog timer
+
+
+	// Setup the red LED.
+	P1->DIR |= LED; // Set LED as output.
+	P1->OUT &= ~LED; // Turn off LED
+
+	P2->DIR |= (LED2RED|LED2GREEN|LED2BLUE);
+	P2->OUT &= ~(LED2RED|LED2GREEN|LED2BLUE);
+
+	while(1)
+	{
+	    if (rand() %17 == 0)
+	        P1->OUT ^= LED;
+	    if (rand() %17 == 0)
+	        P2->OUT ^= LED2RED;
+	    if (rand() %17 == 0)
+	        P2->OUT ^= LED2GREEN;
+	    if (rand() %17 == 0)
+	        P2->OUT ^= LED2BLUE;
+	    sleep(1);
+	}
+
+}
+
+void game()
+{
+    int points = 0;
+    while(points < 10)
+    {
+        rand();
+    }
+}
+
+
+void restart()
+{
+
+}
